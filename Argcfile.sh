8671a9f852b31029dbbe1b46d1d08ccc67586237
onspli <ondrej.splichal@gmail.com>
2026-03-15 22:59:59 +0100

    argc: add test command

--- Argcfile.sh
+++ Argcfile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-modules=("core" "model" "contact" "forward" "rule_filter" "fcm" "email_recovery" "server_settings" "scribe" "labels")
+modules=("core" "model" "contact" "forward" "rule_filter" "fcm" "email_recovery" "server_settings" "labels" ".")
 
 # @cmd Run code generation and extract ARB translation templates.
 codegen() {
@@ -13,21 +13,20 @@ codegen() {
     )
     done
 
-    # For the parent module
-    flutter pub get
-    dart run build_runner build --delete-conflicting-outputs &&
-
     # Localozations
     dart run intl_generator:extract_to_arb --suppress-last-modified --output-dir=./lib/l10n lib/main/localizations/app_localizations.dart &&
         dart run intl_generator:generate_from_arb --output-dir=lib/l10n --no-use-deferred-loading lib/main/localizations/app_localizations.dart lib/l10n/intl*.arb
 
-    # For scribe module localizations
-    (
-    cd scribe
-    dart run intl_generator:extract_to_arb --suppress-last-modified --output-dir=./lib/scribe/ai/l10n lib/scribe/ai/localizations/scribe_localizations.dart &&
-        dart run intl_generator:generate_from_arb --output-dir=lib/scribe/ai/l10n --no-use-deferred-loading lib/scribe/ai/localizations/scribe_localizations.dart lib/scribe/ai/l10n/intl*.arb
-    )
+}
 
+# @cmd Run unit tests.
+test() {
+    for mod in "${modules[@]}"; do
+    (
+        cd "$mod"
+        flutter test
+    )
+    done
 }
 
 eval "$(argc --argc-eval "$0" "$@")"
