485160628c975814a5b0e829665c29a802bf7011
onspli <ondrej.splichal@gmail.com>
2026-03-19 03:08:07 +0100

    test:  storage_service_impl_memory

---
+++ test/data/storage/storage_service_impl_memory_test.dart
@@ -0,0 +1,79 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:leithmail/data/storage_service_impl_memory.dart';
+
+void main() {
+  late StorageServiceImplMemory storage;
+
+  setUp(() {
+    storage = StorageServiceImplMemory();
+  });
+
+  group('read/write', () {
+    test('write and read returns value', () async {
+      await storage.write('key', 'value');
+      expect(await storage.read('key'), 'value');
+    });
+
+    test('read returns null for missing key', () async {
+      expect(await storage.read('missing'), isNull);
+    });
+
+    test('write overwrites existing value', () async {
+      await storage.write('key', 'value1');
+      await storage.write('key', 'value2');
+      expect(await storage.read('key'), 'value2');
+    });
+  });
+
+  group('delete', () {
+    test('delete removes key', () async {
+      await storage.write('key', 'value');
+      expect(await storage.read('key'), 'value');
+      await storage.delete('key');
+      expect(await storage.read('key'), isNull);
+    });
+
+    test('delete non-existing key does not throw', () async {
+      expect(() => storage.delete('missing'), returnsNormally);
+    });
+  });
+
+  group('readAll', () {
+    test('returns all written keys', () async {
+      await storage.write('key1', 'value1');
+      await storage.write('key2', 'value2');
+      expect(await storage.readAll(), {'key1': 'value1', 'key2': 'value2'});
+    });
+
+    test('returns empty map when nothing written', () async {
+      expect(await storage.readAll(), isEmpty);
+    });
+
+    test('readAll returns unmodifiable map', () async {
+      await storage.write('key', 'value');
+      final all = await storage.readAll();
+      expect(() => all['key'] = 'modified', throwsUnsupportedError);
+    });
+  });
+
+  group('deleteAll', () {
+    test('deletes all keys', () async {
+      await storage.write('key1', 'value1');
+      await storage.write('key2', 'value2');
+      await storage.deleteAll();
+      expect(await storage.readAll(), isEmpty);
+    });
+
+    test('deleteAll does not throw when empty', () async {
+      expect(() => storage.deleteAll(), returnsNormally);
+    });
+  });
+
+  group('isolation', () {
+    test('two instances do not share state', () async {
+      final other = StorageServiceImplMemory();
+      await storage.write('key', 'value');
+      expect(await other.read('key'), isNull);
+    });
+  });
+}
